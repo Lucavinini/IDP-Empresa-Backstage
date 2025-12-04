25020c0e0356c851a353c286798d98ab137d1b9f
Lucavinini <lucas.00000850855@unicap.br>
2025-12-04 18:01:20 -0300

    feat: adiciona template de MicroService com Express e Node

---
+++ examples/express-template/content/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'Tudo certo!', timestamp: new Date() });
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor ouvindo a porta: ${port}`);
+});
